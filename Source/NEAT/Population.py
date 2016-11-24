0523035c1fd1059b42e043185917ce8980ec78ba
Thomas-Gu <thomasguilmeau@hotmail.fr>
2016-11-24 15:56:59 +0100

    Le squelette y est
    Revoir les attributs dans la définition ou l'utilisation des objets

--- Source/NEAT/Population.py
+++ Source/NEAT/Population.py
@@ -21,25 +21,62 @@ class Population():
     def genrand(self, max, nb_entr, nb_sort):
         for i in range (self.length):
             self.contenu.append(genome([nb_entr, nb_sort], 'generer'))
+        
             
     def fusionrand(self, ind1, ind2):
         f1 = fitness(ind1)
         f2 = fitness(ind2)
-        l = []
+        
+        entr = ind1.entrees
+        sort = ind1.sorties #tous les individus ont les mêmes listes d'entrées et de sorties
+        
+        lc = [] #Cette liste contient toutes les connexions du fils
+        ln = [] #Cette liste contient tous les noeuds du fils
         for i in ind1.connexions:
             for j in ind2.connexions:
                 if i.innovation == j.innovation :
                     if not(i.activation) or not(j.activation) :
                         if rd.random() < 0.5 :
-                            l.append(i)
+                            lc.append(i)
+                            ln.append(i.entree)
+                            ln.append(i.sortie)
                         else : 
-                            l.append(j)
+                            lc.append(j)
+                            ln.append(j.entree)
+                            ln.append(j.sortie)
                     if rd.random() < 0.5 :
-                        l.append(i)
+                        lc.append(i)
+                        ln.append(i.entree)
+                        ln.append(i.sortie)
                     else :
-                        l.append(j)
+                        lc.append(j)
+                        ln.append(j.entree)
+                        ln.append(j.sortie)
                 else:
                     if f1>f2:
-                        l.append(i)
-                        
+                        if rd.random() < 0.5 :
+                            lc.append(i)
+                            ln.append(i.entree)
+                            ln.append(i.sortie)
+                    else :
+                        if rd.random() < 0.5 :
+                            lc.append(j)
+                            ln.append(j.entree)
+                            ln.append(j.sortie)
+        #On connait toutes les connexions, ainsi que tous les noeuds impliqués.
+        #Cependant il y a des doublons dans la liste des noeuds
+        #Pour les enlever, tri puis suppression
+        ln2 = sorted(ln, key=lambda noeud: noeud.id)
+        n = len(ln2)
+        doublons = []
+        for i in range (n-1):
+            if ln2(i) == ln(i+1):
+                doublons.append(i)
+        for i in doublons :
+            ln2.remove(i)
+        
+        fils = Genome(entr, sort, ln2)
+        return fils 
+        
+        
         
