b56b880ea6efd108621b0dd56b8bc6423569ac7a
BadrYoubiIdrissi <badryoubiidrissi@gmail.com>
2017-01-03 22:50:54 +0100

    Revert "rajouté des fonctions d'éliminations de doublons"
    This reverts commit a76410c9377c466d33739493d89666f708900f17.

--- Source/NEAT/utilitaires.py
+++ Source/NEAT/utilitaires.py
@@ -5,7 +5,6 @@ Created on Sun Dec 11 09:42:00 2016
 Fonctions utiles:
     -Offset middle: Renvoie les coordonnés du milieu décalé de offset (utile pour affichage bezier)
     -Sigmoide: renvoie la sigmoide d'un vecteur (ou une matrice) terme à terme
-    -elimine_doublons: prend une liste en argument et renvoie la même liste sans ses éléments en double
 
 @author: byoub
 """
@@ -23,25 +22,3 @@ def offsetMiddle(pos1,pos2,off=15):
 
 def sigmoide(vect):
     return (1/(1+np.exp(-vect)))
-
-    
-def elimine_doubl_int(liste):
-    l = sorted(liste)
-    i = 0
-    while i < len(l)-1 :
-        if l[i] == l[i+1]:
-                l.pop(i)
-        else :
-            i += 1   
-    return l
-    
-def elimine_doubl_co(liste):
-    l = sorted(liste, key = lambda connexion : connexion.entree)
-    i = 0
-    while i < len(l)-1:
-        if l[i].innovation == l[i+1].innovation :
-            l.pop(i)
-        else :
-            i += 1
-    return l
-        
